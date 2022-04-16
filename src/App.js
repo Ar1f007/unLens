9b882b9acacfe6cfbcca7c289dd76d6d9f67016b
Ar1f007 <muhammadarif9416@yahoo.com>
2022-04-17 00:13:26 +0600

    integreted tailwind css with daisyUI

--- src/App.js
+++ src/App.js
@@ -2,16 +2,9 @@ import React from 'react';
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h1 className="text-3xl font-bold underline">
+      Hello world! <button class="btn">Button</button>
+    </h1>
   );
 }
 
