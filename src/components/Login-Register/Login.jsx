7208b2683d6079493fa4e01a7922d2863d195137
Ar1f007 <muhammadarif9416@yahoo.com>
2022-04-17 23:25:30 +0600

    firebase setup

--- src/components/Login-Register/Login.jsx
+++ src/components/Login-Register/Login.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 export const Login = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:w-8/12">
-        <h1 className="text-5xl font-bold">Login now!</h1>
+      <div className="hero-content flex-col w-full lg:w-8/12">
+        <h1 className="text-3xl lg:text-5xl font-bold">Login now!</h1>
 
         <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
           <div className="card-body">
@@ -19,7 +19,7 @@ export const Login = () => {
                 <span className="label-text">Password</span>
               </label>
               <input type="text" placeholder="password" className="input input-bordered" />
-              <label className="label">
+              <label className="label flex flex-col lg:flex-row items-start gap-y-1">
                 <Link to="/forgot-password" className="label-text-alt link link-hover">
                   Forgot password?
                 </Link>
@@ -28,7 +28,7 @@ export const Login = () => {
                 </Link>
               </label>
             </div>
-            <div className="form-control mt-6">
+            <div className="form-control mt-3">
               <button className="btn btn-primary">Login</button>
             </div>
           </div>
--- src/components/Login-Register/Register.jsx
+++ src/components/Login-Register/Register.jsx
@@ -3,8 +3,8 @@ import { Link } from 'react-router-dom';
 export const Register = () => {
   return (
     <div className="hero min-h-screen bg-base-200">
-      <div className="hero-content flex-col lg:w-8/12">
-        <h1 className="text-5xl font-bold">Register here!</h1>
+      <div className="hero-content flex-col w-full lg:w-8/12">
+        <h1 className="text-3xl lg:text-5xl font-bold">Register here!</h1>
 
         <div className="card flex-shrink-0 w-full max-w-lg shadow-2xl bg-base-100">
           <div className="card-body">
